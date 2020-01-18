d36d1a1de986109f1e53575b729d26931a265da6
Sergey Cherednichenko <sergey.cherednichenko@simbirsoft.com>
2020-01-18 13:22:07 +0300

    license fix

--- resources/views/pages/license.blade.php
+++ resources/views/pages/license.blade.php
@@ -24,11 +24,6 @@
 			<div class="license__wrapper">
 				<div class="row">
 					<div class="col-12 col-md-6 col-lg-4">
-						<a href="/img/licences/lic1.jpg" class="license__item popup-img">
-                           <img id="myImg" src="/img/licences/lic1.jpg" class="img-responsive" alt="" style="width:100%;max-width:300px">
-						</a>
-					</div>
-					<div class="col-12 col-md-6 col-lg-4">
 						<a href="/img/licences/lic2.jpg" class="license__item popup-img">
                            <img src="/img/licences/lic2.jpg" class="img-responsive" alt="" style="width:100%;max-width:300px">
 						</a>
@@ -48,7 +43,7 @@
 							<img src="/img/licences/lic_dpo.jpg" class="img-responsive" alt="" style="width:100%;max-width:300px">
 						</a>
 					</div>
-
+				<div class="row">
 					<div class="col-12 col-md-6 col-lg-8">
 						<div class="license__documents">
 							<h3>
